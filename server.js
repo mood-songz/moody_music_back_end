013505a120e83b7b45b7f4457f15940a53154b9d
Kishor Pandey <kishor.jpandey@gmail.com>
2019-05-13 22:29:44 -0700

    testing server.js

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 const app = express();
-const pg = require('pg');
+// const pg = require('pg');
 
-require('dotenv').config();
+// require('dotenv').config();
 app.use(cors());
 
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.static('./public'));
+
+app.get('/itbetterwork', (request,response)=>{
+  response.send('yes');
+});
 
 app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
