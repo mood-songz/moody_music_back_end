071070383ab30b595167871f3e76be5043ee2690
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-16 05:06:59 -0700

    add user endpoints

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 const fs = require('fs');
+
 //face++ api package
 var facepp = require('face-plusplus-node');
-//require session to store keyword which is got from face++ api based on image
-var session = require('express-session');
-app.use(session({secret:'ssshhhhh'}));
+
 //using for image handler
 const multer=require('multer');
 
@@ -18,7 +17,7 @@ const multer=require('multer');
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-
+app.use(express.urlencoded());
 /*************************  image handler *****************************************/
 
 var storage = multer.diskStorage({
@@ -101,6 +100,9 @@ function fillEmotionsTable() {
 }
 
 /*************************  Endpoints  *****************************************/
+app.get('/', function(request, response){
+  response.send('ok');
+});
 
 // gets recommendations for a playlist
 // If we get API to work as we want to, possibly refactor to include userid and valence in string, for example:  app.get('/recommendations/userid/emotion', (request, response) => {
@@ -137,6 +139,62 @@ app.get('/recommendations/:emotion', (request, response) => {
     )
     .catch(error => console.error(error));
 });
+
+//get user from database
+app.get('/users/:username/:email', (request, response) => {
+  let username = request.params.username;
+  let email = request.params.email;
+
+  let selectStatement = `SELECT * FROM users WHERE username = $1 and useremail = $2;`;
+  let values = [username, email];
+
+  try {
+    client.query(selectStatement, values)
+      .then(databaseResponse => {
+        if(databaseResponse.rows.length > 0){
+          let user = databaseResponse.rows[0];
+          response.send({'username': user.username, 'email': user.useremail});
+        } else {
+          response.send({});
+        }
+      });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//save user to database
+app.post('/users/:username/:email', (request, response) => {
+  let username = request.params.username;
+  let email = request.params.email;
+  let insertStatement = `INSERT into users (username, useremail) VALUES ($1, $2);`;
+  let selectStatement = `SELECT * FROM users WHERE username = $1 and useremail = $2;`;
+  let values = [username, email];
+  try {
+    client.query(selectStatement, values)
+      .then(databaseResponse => {
+        if(databaseResponse.rows.length > 0){
+          console.log('user already exists');
+          response.send(false);
+        } else {
+          try {
+            client.query(insertStatement, values)
+              .then(databaseResponse => {
+                if(databaseResponse.rowCount > 0) {
+                  console.log('Saved new user');
+                }
+                response.send(databaseResponse.rowCount > 0);
+              }); 
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 
 /************************* Constructors  *****************************************/
 
